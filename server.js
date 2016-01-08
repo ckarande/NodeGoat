10100dace2a48b583eec62b7630027e6c711bcaa
Chetan Karande <kchetan.tech@gmail.com>
2016-01-08 15:22:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -134,8 +134,7 @@ MongoClient.connect(config.db, function(err, db) {
     });
 
     // Insecure HTTP connection
-    http.createServer(app).listen(config.port, config.hostName, function() {
-        console.log('process.env=', process.env);
+    http.createServer(app).listen(config.port, function() {
         console.log("Express http server listening on port " + config.port);
     });
 
